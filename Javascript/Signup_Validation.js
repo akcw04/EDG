d52d33921bc776f7651c5e13a53bc9d789a2f31c
Annie Kiu Chi Wen <151919987+akcw04@users.noreply.github.com>
2024-04-08 15:40:10 +0800

    updated validation changes

--- Javascript/Signup_Validation.js
+++ Javascript/Signup_Validation.js
@@ -1,43 +1,30 @@
 function formvalid() {
+    var password_val = document.getElementById("Password").value;
 
-    var password_val = document.getElementById("password").value;
-
-    if (password_val.length <= 7 || password_val.length >= 20) {
-      document.getElementById("val-password").innerHTML = " * Minimum 8 characters";
-      return false;
-    } else {
-      document.getElementById("val-password").innerHTML = "";
-      return true;
-    }
-  }
-
-// Get the password and confirm password fields
-const passwordField = document.getElementById('password');
-const confirmPasswordField = document.getElementById('confirmPassword');
-
-// Function to validate the password
-function Validate_Password() {
-
-    var password_val = document.getElementById("password").value;
-    var confirm_password_val = document.getElementById("confirm_password").value;
-
-    if (password_val !== confirm_password_val) {
-        // Passwords do not match
-        document.getElementById("val-confirm").innerHTML = " * Passwords do not Match";
+    if (password_val.length < 8) {
+        document.getElementById("val-password").innerHTML = " * Minimum 8 characters";
         return false;
     } else {
-        // Passwords match
+        document.getElementById("val-password").innerHTML = "";
+        return true;
+    }
+}
+
+function Validate_Password() {
+    var password_val = document.getElementById("Password").value;
+    var confirm_password_val = document.getElementById("Confirm_Password").value;
+
+    if (password_val !== confirm_password_val) {
+        document.getElementById("val-confirm").innerHTML = " * Passwords do not match";
+        return false;
+    } else {
         document.getElementById("val-confirm").innerHTML = "";
         return true;
     }
 }
 
-// Add event listeners to the password and confirm password fields
-passwordField.addEventListener('input', validatePassword);
-confirmPasswordField.addEventListener('input', validatePassword);
-
 function validate_phone_number() {
-    var phone_number = document.getElementById("phone").value;
+    var phone_number = document.getElementById("PhoneNumber").value;
     var phone_number_regex = /^[0-9]{10}$/;
     if (!phone_number.match(phone_number_regex)) {
         document.getElementById("val-phone").innerHTML = " * Invalid Phone Number";
@@ -49,7 +36,7 @@ function validate_phone_number() {
 }
 
 function validate_email() {
-    var email = document.getElementById("email").value;
+    var email = document.getElementById("Email").value;
     var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (!email.match(email_regex)) {
         document.getElementById("val-email").innerHTML = " * Invalid Email";
