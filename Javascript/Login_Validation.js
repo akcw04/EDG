3cb087d1e369a915638094286168945210ba3e2e
Annie Kiu Chi Wen <151919987+akcw04@users.noreply.github.com>
2024-05-29 18:49:55 +0800

    view password for login and signup

--- Javascript/Login_Validation.js
+++ Javascript/Login_Validation.js
@@ -24,3 +24,13 @@ function formvalid() {
         return true;
     }
 }
+
+function togglePasswordVisibility(id) {
+  var input = document.getElementById(id);
+  if (input.type === "password") {
+      input.type = "text";
+  } else {
+      input.type = "password";
+  }
+}
+
